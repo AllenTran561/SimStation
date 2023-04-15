1f9395202814d2f095566e68dbd0a5b0e6bd37c3
Allen <allentran561@gmail.com>
2023-04-14 19:00:20 -0700

    View is working

--- src/simstation/SimStationView.java
+++ src/simstation/SimStationView.java
@@ -26,9 +26,7 @@ public class SimStationView extends View {
         g2d.setColor(new Color(50, 50, 50));
         g2d.fillRect(0, 0,  width,  height);
         g2d.setColor(new Color(50, 200, 100));
-        //System.out.println(agents);
         for (Agent a : agents) {
-            System.out.println("agent");
             gc.setColor(Color.RED);
             gc.fillOval(a.getX_Pos(), a.getY_Pos(), sim.getDotSize(), sim.getDotSize());
         }
